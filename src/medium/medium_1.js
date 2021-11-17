dce1d468701164124a7c5cdb2d16731e0e5eb37b
Jillian Anderson <59696481+jilland@users.noreply.github.com>
2021-11-17 12:05:32 -0500

    fixing formatting for getsum

--- src/medium/medium_1.js
+++ src/medium/medium_1.js
@@ -12,7 +12,7 @@ export function getSum(array) {
     for(let i=0; i<array.length; i++){
         sum = sum + array[i];
     }
-    return {sum}
+    return sum;
 }
 
 
@@ -26,7 +26,7 @@ export function getSum(array) {
  * console.log(getMedian(array)); // 4.5
  */
 export function getMedian(array) {
-    
+
 }
 
 /**
