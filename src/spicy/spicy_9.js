500eec36cb878f95dadb4953a20c1195291e09b7
Jillian Anderson <59696481+jilland@users.noreply.github.com>
2021-11-20 16:58:06 -0500

    editing someEven

--- src/spicy/spicy_9.js
+++ src/spicy/spicy_9.js
@@ -122,8 +122,8 @@ export const everyEven = (arr, test) => {
  *    someEven([0, 0, 0, 0, 0], x => x === 0)  <--  returns true
  */
 export const someEven = (arr, test) => {
-    for(let i=0; i<arr.length; i = i + 2){
-        if(test(arr[i])) {
+    for(let i=0; i<arr.length; i++){
+        if(i % 2 == 0 && test(arr[i])) {
             return true;
         }
     }
